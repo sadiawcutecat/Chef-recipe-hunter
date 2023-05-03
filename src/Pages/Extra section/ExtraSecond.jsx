c3bf1cf77e76409f6590fcdc9088ba64639f88e4
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-05-03 09:35:55 +0600

    extra section second design completed

--- src/Pages/Extra section/ExtraSecond.jsx
+++ src/Pages/Extra section/ExtraSecond.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-// import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const ExtraSecond = () => {
     return (
@@ -7,12 +7,12 @@ const ExtraSecond = () => {
             <div className=' mt-5 p-5'>
                 <h3 className='text-success'>HI, I AM ALEX.</h3>
                 <p className='my-5'>I am a food stylist & photographer. Loves nature and healthy food, and good coffee. I am here to help you cook delicious and healthy <br /> food with easy to follow step by step tutorial. Don’t hesitate to <br /> come for say a small “hello!”</p>
-                {/* <Button variant="success" size="lg">
+                <Button variant="success" size="lg">
                     More About Me
-                </Button> */}
+                </Button>
             </div>
             <div className='p=4'>
-                {/* <img className='h=50 w-100  ' src="/src/assets/images/about-photos.jpg" alt="" /> */}
+                <img className='h=50 w-100  ' src="/src/assets/images/about-photos.jpg" alt="" />
             </div>
         </div>
     );
